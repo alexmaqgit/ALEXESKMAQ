4bb20fbe34323bd9a5bbcdfbf587c5ac645e0ad8
alexmaqgit <alexmaqgit@github.com>
2026-04-10 16:03:02 +0300

    زيادة timeout وتقليل threads

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 # gunicorn.conf.py
 workers = 1  # قلل من 2 إلى 1
 threads = 1  # قلل threads
-timeout = 120  # زيد timeout
+timeout = 300  # زدها من 120 إلى 300
 max_requests = 100  # أعد تشغيل العامل بعد 100 طلب
